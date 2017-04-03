570b7c5461e0e4018f0cd5d634a3b258c5b47634
Pete Rossman <pete.rossman@gmail.com>
2017-04-03 14:32:32 -0400

    First commit

---
+++ GC_BonusLab6_StudentScores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_BonusLab6_StudentScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = 1;
+            Console.WriteLine("Welcome to the Student Scores Application!");
+            Console.WriteLine("\nEnter number of students: ");
+            int.TryParse(Console.ReadLine(), out students);
+            Student[] list = new Student[students];
+            for (int i = 0; i < students; i++)
+            {
+                Console.Write("\nStudent " + (i + 1) + " last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("\nStudent " + (i + 1) + " first name: ");
+                string firstName = Console.ReadLine();
+                Console.Write("\nStudent " + (i + 1) + " score: ");
+                int score = int.Parse(Console.ReadLine());
+                list[i] = new Student(firstName, lastName, score);
+            }
+            foreach (Student s in list)
+            {
+                Console.WriteLine(s.LastName + ", " + s.FirstName + ": " + s.Score);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GC_BonusLab6_StudentScores/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_BonusLab6_StudentScores
+{
+    public class Student
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public int Score { set; get; }
+
+        public Student (string firstName, string lastName, int score)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Score = score;
+        }
+    }
+}
