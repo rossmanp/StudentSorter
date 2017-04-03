49d790955d46cf998ad888a0daf4ddae37128098
Pete Rossman <pete.rossman@gmail.com>
2017-04-03 14:53:12 -0400

    Validator class now checks if a string is null or white space

--- GC_BonusLab6_StudentScores/Program.cs
+++ GC_BonusLab6_StudentScores/Program.cs
@@ -9,20 +9,19 @@ namespace GC_BonusLab6_StudentScores
     class Program
     {
         static void Main(string[] args)
-        {
-            int students = 1;
+        {           
             Console.WriteLine("Welcome to the Student Scores Application!");
             Console.WriteLine("\nEnter number of students: ");
-            int.TryParse(Console.ReadLine(), out students);
+            int students = Validator.getIntWithinRange(1, 100);           
             Student[] list = new Student[students];
             for (int i = 0; i < students; i++)
             {
                 Console.Write("\nStudent " + (i + 1) + " last name: ");
-                string lastName = Console.ReadLine();
+                string lastName = Validator.getString(Console.ReadLine());
                 Console.Write("\nStudent " + (i + 1) + " first name: ");
-                string firstName = Console.ReadLine();
+                string firstName = Validator.getString(Console.ReadLine());
                 Console.Write("\nStudent " + (i + 1) + " score: ");
-                int score = int.Parse(Console.ReadLine());
+                int score = Validator.getIntWithinRange(1,100);
                 list[i] = new Student(firstName, lastName, score);
             }
             foreach (Student s in list)
---
+++ GC_BonusLab6_StudentScores/Validator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_BonusLab6_StudentScores
+{
+    class Validator
+    {
+        static int _myInt;
+
+        public Validator()
+        {
+
+        }
+
+        //This method confirms that the user entered an integer, and returns the integer.
+
+        public static int getInt(string prompt)
+        {
+            int.TryParse(prompt, out _myInt);
+            while (!int.TryParse(prompt, out _myInt))
+            {
+                Console.Write("\nThe value must be an integer, try again: ");
+                prompt = Console.ReadLine();
+            }
+            return _myInt;
+        }
+
+        //For the user to proceed, he or she must enter an integer within the specified range.
+
+        public static int getIntWithinRange(int min, int max)
+        {
+            bool run = true;
+            Validator num = new Validator();
+            while (run)
+            {
+                _myInt = Validator.getInt(Console.ReadLine());
+                if (_myInt < min)
+                {
+                    Console.WriteLine("\nError! Please enter an integer greater than " + (min - 1));
+                }
+                else if (_myInt > max)
+                {
+                    Console.WriteLine("\nError! Please enter an integer less than " + (max + 1));
+                }
+                else
+                {
+                    run = false;
+                }
+            }
+            return _myInt;
+        }
+
+        public static string getString(string input)
+        {
+            bool run = true;
+            while (run)
+            {
+                if (String.IsNullOrWhiteSpace(input)) 
+                {
+                    Console.WriteLine("You did not input a name! Please try again.");
+                    return Validator.getString(Console.ReadLine());
+                }
+                else
+                {
+                    run = false;
+                }
+            }
+            return input;
+        }
+    }
+}
